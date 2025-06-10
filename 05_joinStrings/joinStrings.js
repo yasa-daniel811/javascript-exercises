7df5cd3d23a7e4c51edd96f438b5a7bc945accf4
Dan Chandra <daniel.chandra@yasa.com>
2025-06-10 18:07:44 +0100

    Add solution for exercise 5

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -11,7 +11,15 @@
 
 
 
+let firstName = `Carlos`;
+let lastName = `Stevenson`;
+let thisYear = 1965;
+let birthYear = 1947;
 
+let fullName = `${firstName} ${lastName}`;
+let age = thisYear - birthYear;
+
+let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 // Do not change this
 module.exports = {
