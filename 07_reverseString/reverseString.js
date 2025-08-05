c81663ea5aabc564b4fdc57a809d803116ac000d
Dan Chandra <daniel.chandra@yasa.com>
2025-08-05 16:56:36 +0100

    Add solution for exersice 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let i = 0;
+    let reversedString = "";
 
+    while (i < stringToReverse.length) {
+        reversedString += stringToReverse.charAt(stringToReverse.length - i - 1);
+        i++;
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
