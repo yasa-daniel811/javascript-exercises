82e595279ce3f6639e43a2d68509123d278482d0
Dan Chandra <daniel.chandra@yasa.com>
2025-08-07 08:34:45 +0100

    Add solution for exersise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  answer = (fahrenheit - 32) / (9/5);
+  return (Math.round(answer * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  answer = (celsius * (9/5)) + 32;
+  return (Math.round(answer * 10) / 10);
 };
 
 // Do not edit below this line
