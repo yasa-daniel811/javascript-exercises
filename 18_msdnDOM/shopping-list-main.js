9804ba4e8c00c1158dfe629f33be95081dcefc71
Dan Chandra <daniel.chandra@yasa.com>
2025-10-15 09:04:41 +0100

    Progress mdn list exercise

--- 18_msdnDOM/shopping-list-main.js
+++ 18_msdnDOM/shopping-list-main.js
@@ -1,11 +1,22 @@
-const list = document.querySelector("ul");
+const { useDeferredValue } = require("react");
+
+const listContainer = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
 button.addEventListener("click", (event) => {
-    
     event.preventDefault();
+
     const inputText = input.value;
-    alert(inputText);
-    input.textContent = "";
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listButton = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = inputText;
+    listItem.appendChild(listButton);
+    listButton.textContent = 'Delete';
+    listContainer.appendChild(listItem);
 })
