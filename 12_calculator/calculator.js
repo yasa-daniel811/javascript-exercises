1a3fb77df3b19b16ad95befcc3c3e980e53f4a3e
Dan Chandra <daniel.chandra@yasa.com>
2025-10-21 13:25:12 +0100

    Add calculator solution

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(intArray) {
+	return intArray.reduce((sum, int) => sum + int, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(intArray) {
+  return intArray.reduce((product, int) => product * int);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
