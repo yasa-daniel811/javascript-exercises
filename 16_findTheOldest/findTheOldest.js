a2f07aec6d51e088bd3c594cb91aa9bfe97f720e
Dan Chandra <daniel.chandra@yasa.com>
2025-10-22 21:23:28 +0100

    Adds partial solution to findtheoldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const maxName = people.reduce((oldestPerson, currentPerson) => {
+        return (currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (people[oldestPerson].yearOfDeath - people[oldestPerson].yearOfBirth) ? person : oldestPerson;
+    })
+     
 };
 
 // Do not edit below this line
