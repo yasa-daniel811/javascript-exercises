a6e339157807913d983e6fcf15c74d24a5183115
Dan Chandra <daniel.chandra@yasa.com>
2025-10-23 08:32:03 +0100

    Add solution for exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    if (int === 0) return 0;
+    if (int === 1) return 1;
+
+    return (fibonacci(int - 1) + fibonacci(int - 2))
 
 };
+
+//1, 1, 2, 3, 5, 8, 13
 
 // Do not edit below this line
 module.exports = fibonacci;
